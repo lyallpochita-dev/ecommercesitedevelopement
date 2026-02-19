925f30bbb4d7c6599579cde3385ffaf8d7636f67
lyallpochita-dev <lyallpochita@gmail.com>
2026-02-19 23:51:10 +1300

    Fix MongoDB connection string password
    Removed special character from MongoDB password.

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
 # 🔐 MongoDB Connection
-MONGO_URL = "mongodb+srv://mongoadmin:Password%123@shivanimongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
+MONGO_URL = "mongodb+srv://mongoadmin:Password123@shivanimongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
 
 client = AsyncIOMotorClient(MONGO_URL)
 db = client.ecommerceDB
